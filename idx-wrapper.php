265221422d8ac981f574e9b09b2b1696717854f1
amcn84 <allen.r.mcnichols@gmail.com>
2016-02-16 15:18:03 -0800

    Create idx-wrapper.php
    Initial commit

---
+++ idx-wrapper.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Plugin Name: IMPress IDX Broker Wrapper Shortcode
+Description: Use [wrapper] on a page to create a dynamic wrapper
+Version: 1.0.0
+Author: Allen McNichols
+Contributors: IDX, LLC
+License: GPLv2 or later
+*/
+function dynWrapper_short_code() {
+	return '<div id="idxStart"></div><br/><div id="idxStop"></div>';
+}
+add_shortcode('wrapper','dynWrapper_short_code');
+?>
