b95a288c93352dadeca2442cdbd1dbe4cd2e5769
amcn84 <allen.r.mcnichols@gmail.com>
2016-02-17 13:06:57 -0800

    Update idx-wrapper.php

--- idx-wrapper.php
+++ idx-wrapper.php
@@ -1,14 +1,24 @@
 <?php
 /*
 Plugin Name: IMPress IDX Broker Wrapper Shortcode
-Description: Use [wrapper] on a page to create a dynamic wrapper
-Version: 1.0.0
-Author: Allen McNichols
-Contributors: IDX, LLC
+Description: Use ['wrapper'] on a page to create a dynamic wrapper
+Version: 1.0.1
+Author: Allen M.
+Contributors: allenm
 License: GPLv2 or later
 */
+function idxHeaders() {
+	echo '<meta name="idx-robot">';
+	echo "\r\n";
+	echo '<meta name="robots" content="noindex,follow"/>';
+	echo "\r\n";
+}
+add_action('wp_head','idxHeaders',1,1);
 function dynWrapper_short_code() {
-	return '<div id="idxStart"></div><div id="idxStop"></div>';
+	echo "\r\n";
+	echo '<div id="idxStart"></div>';
+	echo "\r\n";
+	echo '<div id="idxStop"></div>';
 }
 add_shortcode('wrapper','dynWrapper_short_code');
 ?>
